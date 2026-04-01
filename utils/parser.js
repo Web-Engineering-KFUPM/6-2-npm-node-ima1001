aed649f8fd1bf97559bf24af9b3c023a0bee7f8e
Fatimah <s202248340@kfupm.edu.sa>
2026-04-01 13:24:43 +0300

    t5

--- utils/parser.js
+++ utils/parser.js
@@ -1,8 +1,10 @@
 import _ from "lodash"
 export function parseNumbers(input) {
-            return {}
-          }
+    const numbers = _.map(input, (str) => Number(str));
+    return _.compact(numbers);          
+}
 
 export function isValidOperation(operation) {
-return {}
+    const validOps = ["add", "subtract", "multiply", "divide"];
+    return _.includes(validOps, operation);
 }
